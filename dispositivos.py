ab9c5cfb7ebbd32b2f276662003378137801a89f
jvt1963 <39003362+jvt1963@users.noreply.github.com>
2019-01-26 12:09:37 +0100

    Update and rename devices.py to dispositivos.py

---
+++ dispositivos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+dispositivo = list()
+dispositivoOn = list()
+dispositivoOff = list()
+
+dispositivo.append("luz de la cocina")
+dispositivoOn.append("http://192.168.1.220:8080/json.htm?type=command&param=switchlight&idx=23&switchcmd=On")
+dispositivoOff.append("http://192.168.1.220:8080/json.htm?type=command&param=switchlight&idx=23&switchcmd=Off")
+
+dispositivo.append("luz del dormitorio")
+dispositivoOn.append("http://192.168.1.220:8080/json.htm?type=command&param=switchlight&idx=19&switchcmd=On")
+dispositivoOff.append("http://192.168.1.220:8080/json.htm?type=command&param=switchlight&idx=19&switchcmd=Off")
+
+# Add new dispositivos 
+# dispositivo.append([dispositivo name, slot])
+# dispositivoOn.append([url On])
+# dispositivoOff.append([url Off])
+
