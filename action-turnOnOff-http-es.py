989e7adfa3a9df29f408d4750934aceb0d32cb35
jvt1963 <39003362+jvt1963@users.noreply.github.com>
2019-01-26 12:07:56 +0100

    Update action-turnOnOff-http-es.py

--- action-turnOnOff-http-es.py
+++ action-turnOnOff-http-es.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes 
 import requests
-from devices import device, deviceOn, deviceOff
+from dispositivos import dispositivo, dispositivoOn, dispositivoOff
 
 MQTT_IP_ADDR = "localhost" 
 MQTT_PORT = 1883 
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT)) 
 
 
-def device_number(current_slot):
-    for x in device:
+def dispositivo_number(current_slot):
+    for x in dispositivo:
         if x == current_slot:
             break
-    return device.index(x)
+    return dispositivo.index(x)
 
 
 def intent_received(hermes, intent_message):
     sentence = 'Me has pedido '
-    device_name_slot = intent_message.slots.device_name.first()
-    devnum = device_number(device_name_slot.value)
+    dispositivo_name_slot = intent_message.slots.dispositivo_name.first()
+    devnum = dispositivo_number(dispositivo_name_slot.value)
 
     if intent_message.intent.intent_name == 'jaimevegas:Encender':
-        sentence += 'que encienda ' + device_name_slot.value
-        r = requests.get(deviceOn[devnum])
+        sentence += 'que encienda ' + dispositivo_name_slot.value
+        r = requests.get(dispositivoOn[devnum])
         
     elif intent_message.intent.intent_name == 'jaimevegas:Apagar':
-        sentence += 'que apague ' + device_name_slot.value
-        r = requests.get(deviceOff[devnum])
+        sentence += 'que apague ' + dispositivo_name_slot.value
+        r = requests.get(dispositivoOff[devnum])
         
     else:
         return
